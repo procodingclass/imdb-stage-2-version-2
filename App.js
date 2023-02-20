41b819f9c8b7985fe9f08a859b82870eccf03fbc
vishalgddam873 <vishal18@navgurukul.org>
2023-02-20 18:58:21 +0530

    Initial comment

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import HomeScreen from "./screens/Home";
+import RecommendedMoviesScreen from "./screens/Recommendation";
+import PopularMoviesScreen from "./screens/Popular";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default class App extends Component {
+	render() {
+		return (
+			<NavigationContainer>
+				<AppStackNavigator />
+			</NavigationContainer>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tab = createMaterialTopTabNavigator();
+
+const AppTopNavigation = () => {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen
+				name='RecommendedMovies'
+				component={RecommendedMoviesScreen}
+				options={{
+					tabBarLabelStyle: { color: "#000" },
+					tabBarItemStyle: { width: 100 },
+					tabBarStyle: { backgroundColor: "#fff" },
+					tabBarLabel: "Recommended",
+				}}
+			/>
+			<Tab.Screen
+				name='PopularMovies'
+				component={PopularMoviesScreen}
+				options={{
+					tabBarLabelStyle: { color: "#000" },
+					tabBarItemStyle: { width: 100 },
+					tabBarStyle: { backgroundColor: "#fff" },
+					tabBarLabel: "Popular",
+				}}
+			/>
+		</Tab.Navigator>
+	);
+};
+
+const Stack = createStackNavigator();
+
+function AppStackNavigator() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen
+				name='Home'
+				component={HomeScreen}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name='AppTopNav'
+				component={AppTopNavigation}
+				options={{
+					headerBackTitle: null,
+					headerTintColor: "#fff",
+					headerTitle: "Recommended Movies",
+					headerStyle: {
+						backgroundColor: "#d500f9",
+					},
+					headerTitleStyle: {
+						color: "#fff",
+						fontWeight: "bold",
+						fontSize: RFValue(18),
+					},
+				}}
+			/>
+		</Stack.Navigator>
+	);
+}
---
+++ screens/Popular.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import {
+	View,
+	StyleSheet,
+	FlatList,
+	Text,
+	ImageBackground,
+} from "react-native";
+import axios from "axios";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default class PopularMoviesScreen extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [],
+		};
+	}
+
+	componentDidMount() {
+		this.getData();
+	}
+
+	timeConvert(num) {
+		var hours = Math.floor(num / 60);
+		var minutes = num % 60;
+		return `${hours} hrs ${minutes} mins`;
+	}
+
+	getData = () => {
+		const url = "http://localhost:5000/popular-movies";
+		axios
+			.get(url)
+			.then(async (response) => {
+				this.setState({ data: response.data.data });
+			})
+			.catch((error) => {
+				console.log(error.message);
+			});
+	};
+
+	keyExtractor = (item, index) => index.toString();
+
+	renderItems = ({ item, index }) => {
+		return (
+			<View style={styles.cardContainer}>
+				<ImageBackground
+					source={{ uri: item.poster_link }}
+					imageStyle={styles.cardBackground}
+					style={styles.cardBackgroundContainer}>
+					<Text style={styles.title}>{item.title}</Text>
+					<Text style={styles.subtitle}>
+						{item.release_date.split("-")[0]} |{" "}
+						{this.timeConvert(item.duration)}
+					</Text>
+				</ImageBackground>
+			</View>
+		);
+	};
+
+	render() {
+		const { data } = this.state;
+		return (
+			<View style={styles.container}>
+				<FlatList
+					data={data}
+					keyExtractor={this.keyExtractor}
+					renderItem={this.renderItems}
+				/>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+	},
+	title: {
+		color: "#fff",
+		alignSelf: "flex-start",
+		paddingLeft: RFValue(15),
+		fontSize: RFValue(20),
+		marginTop: RFValue(60),
+		fontWeight: "bold",
+	},
+	subtitle: {
+		fontWeight: "bold",
+		alignSelf: "flex-start",
+		paddingLeft: RFValue(15),
+		fontSize: RFValue(15),
+		color: "#fff",
+	},
+	cardContainer: {
+		height: 150,
+		width: "90%",
+		alignSelf: "center",
+		marginVertical: "5%",
+		borderRadius: RFValue(20),
+	},
+	cardBackgroundContainer: {
+		flex: 1,
+		justifyContent: "center",
+	},
+	cardBackground: {
+		flex: 1,
+		borderRadius: RFValue(20),
+		resizeMode: "cover",
+	},
+});
